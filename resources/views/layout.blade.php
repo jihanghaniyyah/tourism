b80be348cccc3aa6533585229cbb22f3f8fcb224
jihanghaniyyah <72118791+jihanghaniyyah@users.noreply.github.com>
2023-04-11 00:32:45 +0700

    feat landing

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,24 +8,28 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('items.index') }}">Tourism App</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <div>
+                <a class="navbar-brand" href="/" style="font-weight: 600">Tourism App</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+            </div>
+            <div>
                 <div class="collapse navbar-collapse" id="navbarScroll">
                     <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('items.index') }}">Items</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('items.index') }}">Destination</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('item-categories.index') }}">Categories</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('item-categories.index') }}">Category</a>
                         </li>
                     </ul>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>
 
         <div class="container">
             @yield('content')
